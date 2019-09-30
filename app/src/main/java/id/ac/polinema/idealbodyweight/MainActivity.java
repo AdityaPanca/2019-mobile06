0e386b6144385a0e24a3af4de9c6f647ca4d3b65
AdityaPanca <aditya.annecdo@gmail.com>
2019-09-30 11:26:03 +0700

    About Fragment

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -3,6 +3,7 @@ package id.ac.polinema.idealbodyweight;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,21 +11,32 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
 	// Deklarasikan atribut Fragment di sini
+	private AboutFragment aboutFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		aboutFragment = AboutFragment.newInstance("Aditya Panca Putra");
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return  true;
+
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+		if (item.getItemId() == R.id.menu_about) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, aboutFragment)
+					.addToBackStack(null)
+					.commit();
+		}
 		// TODO: Tambahkan penanganan menu di sini
 
 		return super.onOptionsItemSelected(item);
